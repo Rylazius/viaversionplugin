5aea0ee64a4bc2a23347e0d473dcd4c60293c8cc
toidicakhia <toidicakhia1234@gmail.com>
2023-10-08 08:41:45 +0700

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+# ViaForge
+## For more information switch to the master branch
+Startup VM Options: -Dfml.coreMods.load=de.florianmichael.viaforge.mixin.MixinLoader
---
+++ src/main/java/net/minusmc/viaversionplugin/ViaVersionPlugin.kt
@@ -0,0 +1,9 @@
+package me.minusmc.viaversonplugin
+
+import net.minusmc.minusbounce.plugin.Plugin
+
+class ViaVersionPlugin: Plugin("ViaVersionPlugin", "20231007") {
+	override fun init() {
+		println("This is an example mod!!!")
+	}
+}
