07864e079fe2f5ec33834649dd24319431f1f08b
toidicakhia <toidicakhia1234@gmail.com>
2023-10-09 19:25:14 +0700

    fix

--- src/main/java/net/minusmc/viaversionplugin/injection/mixins/MixinGuiMultiplayer.java
+++ src/main/java/net/minusmc/viaversionplugin/injection/mixins/MixinGuiMultiplayer.java
@@ -27,7 +27,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
-
+import java.util.ArrayList;
 @Mixin(GuiMultiplayer.class)
 public class MixinGuiMultiplayer extends GuiScreen {
 
