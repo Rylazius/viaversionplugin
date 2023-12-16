eb6f2076bddabd0eb15f1cc584f9e38772c79c5e
toidicakhia <toidicakhia1234@gmail.com>
2023-12-16 10:21:27 +0700

    fix

--- src/main/java/cc/paimon/viamcp/loader/MCPViaLoader.java
+++ src/main/java/cc/paimon/viamcp/loader/MCPViaLoader.java
@@ -5,14 +5,14 @@ import com.viaversion.viaversion.api.Via;
 import com.viaversion.viaversion.api.connection.UserConnection;
 import com.viaversion.viaversion.api.platform.ViaPlatformLoader;
 import com.viaversion.viaversion.api.protocol.version.VersionProvider;
-import com.viaversion.viaversion.bungee.providers.BungeeMovementTransmitter;
+import com.viaversion.viaversion.bukkit.providers.BukkitViaMovementTransmitter;
 import com.viaversion.viaversion.protocols.base.BaseVersionProvider;
 import com.viaversion.viaversion.protocols.protocol1_9to1_8.providers.MovementTransmitterProvider;
 
 public class MCPViaLoader implements ViaPlatformLoader {
     @Override
     public void load() {
-        Via.getManager().getProviders().use(MovementTransmitterProvider.class, new BungeeMovementTransmitter());
+        Via.getManager().getProviders().use(MovementTransmitterProvider.class, new BukkitViaMovementTransmitter());
         Via.getManager().getProviders().use(VersionProvider.class, new BaseVersionProvider() {
             @Override
             public int getClosestServerProtocol(final UserConnection connection) throws Exception {
