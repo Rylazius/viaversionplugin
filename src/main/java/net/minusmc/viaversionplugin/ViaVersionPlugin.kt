8f4d8a8f83dc1e052e3737601f7634ee7c61bd43
rylazius <94881672+Rylazius@users.noreply.github.com>
2024-01-10 20:55:01 +0700

    Delete src/main/java/net/minusmc/viaversionplugin/ViaVersionPlugin.kt

--- src/main/java/net/minusmc/viaversionplugin/ViaVersionPlugin.kt
+++
@@ -1,8 +0,0 @@
-package me.minusmc.viaversonplugin
-
-import net.minusmc.minusbounce.plugin.Plugin
-import net.minusmc.minusbounce.plugin.PluginAPIVersion
-
-class ViaVersionPlugin: Plugin("ViaVersionPlugin", version = "0.2", minApiVersion = PluginAPIVersion.VER_01) {
-	
-}
