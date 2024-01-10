b75fd4e8f0031320e3e0f54fe6615e8dd017b764
toidicakhia <toidicakhia1234@gmail.com>
2024-01-10 20:29:08 +0700

    mixin issue

--- src/main/java/net/minusmc/viaversionplugin/injection/forge/mixins/network/MixinNetHandlerPlayClient.java
+++ src/main/java/net/minusmc/viaversionplugin/injection/forge/mixins/network/MixinNetHandlerPlayClient.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Overwrite;
 
-@Mixin(NetHandlerPlayClient.class, priority = 1001)
+@Mixin(value = NetHandlerPlayClient.class, priority = 1001)
 public class MixinNetHandlerPlayClient {
 
 	@Shadow
